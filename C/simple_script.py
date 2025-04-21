7a19ccc8959c91dcfbf007dea50b3214af5b151f
joyanta55 <joyantamishu@gmail.com>
2025-04-21 14:09:01 -0400

    update comments

--- C/simple_script.py
+++ C/simple_script.py
@@ -1,6 +1,6 @@
 import os
 
-# Read the environment variable MY_VAR
+# Read the environment variable MY_VAR_B set from container B
 my_var_value = os.getenv('MY_VAR_B', 'Default Value')  # Default if MY_VAR is not set
 
 print(f"Container C: Hello from Python inside C! The value of MY_VAR_B is: {my_var_value}")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DinD_with_Code
+If you are from my Linked post, welcome back. Here, I will clear up the Docker in Docker concept with the simpleset example. 
