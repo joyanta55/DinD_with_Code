173f1d50248be8568a3aec814b39f4c23fd92a60
joyanta55 <joyantamishu@gmail.com>
2025-04-21 13:57:20 -0400

    init

---
+++ C/simple_script.py
@@ -0,0 +1,7 @@
+import os
+
+# Read the environment variable MY_VAR
+my_var_value = os.getenv('MY_VAR_B', 'Default Value')  # Default if MY_VAR is not set
+
+print(f"Container C: Hello from Python inside C! The value of MY_VAR_B is: {my_var_value}")
+
